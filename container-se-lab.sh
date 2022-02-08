3d9ba491af4e20a1fe8b3701fb197cf5f8d12db9
KCKuhnsCS <92876078+KCKuhnsCS@users.noreply.github.com>
2022-02-08 13:30:09 -0700

    Update container-se-lab.sh

--- container-se-lab.sh
+++ container-se-lab.sh
@@ -1,4 +1,11 @@
+read -p "This script will install and configure Docker and MicroK8s all at once. You'll also have an option to deploy Falcon Container Sensor after MicroK8s is configured...
+Press any key to start."
+
 # Lets remove any docker installations - if any
+read -t 5 -p "First we'll check if Docker is installed and remove old versions.
+Next we'll configure resources and permissions we need for Docker and install the new version."
+echo
+echo
 sudo apt-get remove docker docker-engine docker.io containerd runc
 
 # Update and install packages to use a repository over HTTPS
@@ -29,10 +36,15 @@ newgrp docker
 
 # Test Docker
 docker run hello-world
-echo "You should have seen the Docker Hello World message... if not... something busted."
+echo
+echo
+read -t 5 -p "You should see the welcome message from Docker above..."
+echo
+echo
+
 
 # Install microk8s
-echo "Now we're going to install MicroK8s..."
+read -t 5 -p "Now we'll install MicroK8s..."
 sudo snap install microk8s --classic --channel=1.22/stable
 
 # Add current user to K8s group
@@ -42,7 +54,10 @@ newgrp microk8s
 
 # Wait k8s to be ready
 microk8s status --wait-ready
-echo "You should see a message saying K8's is running..."
+echo
+echo
+read -t 5 -p "You should see that MicroK8s is running above.
+Next we'll configure some resources and permissions for MicroK8s to function."
 
 # Configure the microK8s network
 microk8s enable dns registry istio
@@ -60,6 +75,8 @@ cd ..
 
 # Let's test some K8s commands
 kubectl get nodes
-echo "You should see the current node listed above"
 kubectl get pods
-echo "There shouldn't be any pods yet..."
+echo
+echo
+read -t 5 -p "Above you should see the K8s nodes
+There should be no pods running."
