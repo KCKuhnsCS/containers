f6c0071fb6f3d7403449ebe88bc3e377991a5511
KCKuhnsCS <92876078+KCKuhnsCS@users.noreply.github.com>
2022-02-08 13:42:38 -0700

    Create falcon-container.sh

---
+++ falcon-container.sh
@@ -0,0 +1,17 @@
+echo "Enter your Falcon CID"
+read FALCON_CID
+echo "Enter your Client ID"
+read FALCON_CLIENT_ID
+echo "Enter your Client Secret"
+read FALCON_CLIENT_SECRET
+echo "Enter your Falcon Cloud"
+read FALCON_CLOUD
+
+echo $FALCON_CID
+echo $FALCON_CLIENT_ID
+echo FALCON_CLIENT_SECRET
+echo FALCON_CLOUD
+
+
+export FALCON_CLOUD_API=api.crowdstrike.com
+export FALCON_CONTAINER_VERSION="6.30.0-1301"
